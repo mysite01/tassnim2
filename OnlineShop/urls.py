42fe57f94acfb4f186596c29e2a5678fe8fe1079
Tassnim <s90301@bht-berlin.de>
2024-07-17 16:16:14 +0200

    superuser

--- OnlineShop/urls.py
+++ OnlineShop/urls.py
@@ -34,7 +34,6 @@ from django.views.generic.base import TemplateView
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('home/', HomeBirthdayView.as_view(template_name='home.html'), name='home'),
-   # path('home/', TemplateView.as_view(template_name='home.html'), name='home'),
     path('useradmin/', include('UserAdmin.urls')),
     path('useradmin/', include('django.contrib.auth.urls')),
     path('housings/', include('HolidayHousings.urls')),
